ee1847ef2aad59e3dd4cd84a91b8d5807a3cab1f
konietse <koniet.se@gmail.com>
2019-02-11 13:33:28 +0100

    Create project

---
+++ src/main/java/de/hhu/abschlussprojekt/AbschlussprojektApplication.java
@@ -0,0 +1,14 @@
+package de.hhu.abschlussprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlussprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbschlussprojektApplication.class, args);
+	}
+
+}
+
