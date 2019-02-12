82fab0e93e610bbb17c53cbe553e639b139272d5
SarahLuca <findus.luca@yahoo.de>
2019-02-12 12:35:03 +0100

    Add base.html to simplify html (Sara, Martin, Sarah)

--- src/main/java/de/hhu/sharing/web/SharingController.java
+++ src/main/java/de/hhu/sharing/web/SharingController.java
@@ -1,7 +1,6 @@
 package de.hhu.sharing.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +11,7 @@ public class SharingController {
     @GetMapping("/")
     public String startpage() {
 
-        return "startpage";
+        return "index";
     }
 
 
