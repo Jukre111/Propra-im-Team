8ffc9301ef8e17a99e144ea229109ecded7cbac7
konietse <koniet.se@gmail.com>
2019-02-12 10:48:45 +0100

    Modelling database

--- src/main/java/de/hhu/sharing/model/Item.java
+++ src/main/java/de/hhu/sharing/model/Item.java
@@ -2,17 +2,29 @@ package de.hhu.sharing.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
-
 public class Item {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    private String name;
+    private String description;
+    private int rental;     //per Day
+    private int deposit;
+    private Date startdate;
+    private Date enddate;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private User lender;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private User borrower;
+
 
 }
--- src/main/java/de/hhu/sharing/model/User.java
+++ src/main/java/de/hhu/sharing/model/User.java
@@ -3,6 +3,8 @@ package de.hhu.sharing.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -12,12 +14,17 @@ public class User {
     @GeneratedValue
     private Long id;
     private String name;
+    private String password;
     private String mail;
+    private Date birthdate;
 
     @Embedded
     private Address address;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private List<Item> items;
+    private List<Item> borrowedItems = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Item> lendItems = new ArrayList<>();
 
 }
