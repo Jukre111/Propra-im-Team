77218726de3983aa5ace3838af3eb604704ef43d
konietse <koniet.se@gmail.com>
2019-02-12 13:27:50 +0100

    Add DatabaseInitializer (failing) (Julian, Sebastian)

--- src/main/java/de/hhu/sharing/web/SharingController.java
+++ src/main/java/de/hhu/sharing/web/SharingController.java
@@ -1,13 +1,14 @@
 package de.hhu.sharing.web;
 
+import de.hhu.sharing.data.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class SharingController {
 
+    UserRepository users;
 
     @GetMapping("/")
     public String startpage() {
