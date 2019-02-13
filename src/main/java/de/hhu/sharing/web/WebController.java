c7fe75ab80ab37c35acd881b75b6d30773d6c2ba
konietse <koniet.se@gmail.com>
2019-02-13 15:15:19 +0100

    Add newitem page in Controller (Sarah, Sebastian)

--- src/main/java/de/hhu/sharing/web/WebController.java
+++ src/main/java/de/hhu/sharing/web/WebController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
@@ -45,6 +46,11 @@ public class WebController {
                         () -> new RuntimeException("Item not found!"));
         model.addAttribute("item", item);
         return "details";
+    }
+
+    @GetMapping("/newitem")
+    public String newitemPage(){
+        return "newitem";
     }
 
 
