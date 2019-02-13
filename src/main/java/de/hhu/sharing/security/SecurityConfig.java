8ec6ceac813b6958026a2fb89128a4131d6bce39
Rotfuechsin <johil101@hhu.de>
2019-02-13 11:41:20 +0100

    add encrypter (Sarah, Johanna)

--- src/main/java/de/hhu/sharing/security/SecurityConfig.java
+++ src/main/java/de/hhu/sharing/security/SecurityConfig.java
@@ -16,10 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PersonService personService;
 
-    //@Bean
-    //public PasswordEncoder encoder(){
-    //    return new BCryptPasswordEncoder();
-    //}
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
