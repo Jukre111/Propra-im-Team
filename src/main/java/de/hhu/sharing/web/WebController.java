d1f71cbb40f2198c17e39680da8abccdf3e687ad
Rotfuechsin <johil101@hhu.de>
2019-02-13 14:11:03 +0100

    add detailansicht & modify index.html (Sahara, Johanna)

--- src/main/java/de/hhu/sharing/web/WebController.java
+++ src/main/java/de/hhu/sharing/web/WebController.java
@@ -1,13 +1,18 @@
 package de.hhu.sharing.web;
 
+import de.hhu.sharing.data.ItemRepository;
 import de.hhu.sharing.data.UserRepository;
+import de.hhu.sharing.model.Item;
 import de.hhu.sharing.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class WebController {
@@ -15,9 +20,15 @@ public class WebController {
     @Autowired
     private UserRepository users;
 
+    @Autowired
+    private ItemRepository itemRepository;
+
     @GetMapping("/")
     public String index(Model model, Principal p) {
-        model.addAttribute("username", p.getName());
+
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+
         return "index";
     }
     @GetMapping("/account")
@@ -27,6 +38,19 @@ public class WebController {
                         () -> new RuntimeException("User not found!"));
         model.addAttribute("user",user);
         return "account";
+    }
+    @GetMapping("/detail")
+    public String detail(@RequestParam(name = "id") final String id, Model model){
+        Long thisId = new Long(id);
+        Optional<Item> item = itemRepository.findById(thisId);
+        if(!item.isPresent()){
+            throw new ItemNotFound();
+        }
+        User user = item.get().getLender();
+        model.addAttribute("item", item.get());
+        model.addAttribute("user", user);
+
+        return "index";
     }
 
 
