1b8f3c71c147ef4d9c93f1a0f50dea3dded1e657
Maren <maren.brand@gmx.net>
2019-02-13 13:43:59 +0100

    Add html files for mail service (My Ky, Maren)

--- src/main/java/de/hhu/sharing/web/SharingController.java
+++ src/main/java/de/hhu/sharing/web/SharingController.java
@@ -1,8 +1,11 @@
 package de.hhu.sharing.web;
 
 import de.hhu.sharing.data.UserRepository;
+import de.hhu.sharing.model.Message;
+import de.hhu.sharing.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,8 +15,18 @@ public class SharingController {
     private UserRepository persons;
 
     @GetMapping("/")
-    public String startpage() {
-        return "index";
+    public String startpage(Model model) {
+        Message msg = new Message();
+        User sender = new User();
+        sender.setMail("test@test.test");
+
+        msg.setSubject("Hello,");
+        msg.setMessage("World!");
+        msg.setSender(sender);
+
+        model.addAttribute("message", msg);
+
+        return "displayMessage";
     }
 
     @GetMapping("/login")
