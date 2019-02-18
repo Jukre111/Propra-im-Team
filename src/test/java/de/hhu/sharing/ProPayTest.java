d10f0d0e9f0f1313f73ee68d72353975b7f9bac5
My Ky <m.k.huynh@gmx.de>
2019-02-18 10:59:34 +0100

    Fix exceptions in ProPayService, add a test to ProPayTest (My Ky, Maren)

--- src/test/java/de/hhu/sharing/ProPayTest.java
+++ src/test/java/de/hhu/sharing/ProPayTest.java
@@ -1,7 +1,6 @@
 package de.hhu.sharing;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -9,16 +8,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.client.RestTemplate;
-
-import com.google.gson.Gson;
 
 import de.hhu.sharing.data.ItemRepository;
 import de.hhu.sharing.model.Account;
@@ -32,27 +24,27 @@ import de.hhu.sharing.web.ProPayService;
 public class ProPayTest {
 
 
-	@Autowired
-	ProPayService pps = new ProPayService();
-	@Autowired
-	ItemRepository itemRepo;
+    @Autowired
+    ProPayService pps = new ProPayService();
+    @Autowired
+    ItemRepository itemRepo;
 
-    public Account prepareProPayUseForGet(String json) throws Exception{
-    	String username = "user";
-    	RestTemplate rt = Mockito.mock(RestTemplate.class);
-        Mockito.when(rt.getForObject("http://localhost:8888/account/" +username + "/", String.class))
-        		.thenReturn(json);
-    	pps.changeTemplateTo(rt);
-    	System.out.println();
+    public Account prepareProPayUseForGet(String json) throws Exception {
+        String username = "user";
+        RestTemplate rt = Mockito.mock(RestTemplate.class);
+        Mockito.when(rt.getForObject("http://localhost:8888/account/" + username + "/", String.class))
+                .thenReturn(json);
+        pps.changeTemplateTo(rt);
+        System.out.println();
         Account acc = pps.showAccount(username);
-    	return acc;
+        return acc;
     }
 
     @Test
-    public void checkShowAccountResponse() throws Exception{
-    	Account acc = prepareProPayUseForGet("{\"account\":\"user\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}");
-    	ArrayList<Reservation> reservations = acc.getReservations();
-    	Assertions.assertThat(acc.getAccount()).isEqualTo("user");
+    public void checkShowAccountResponse() throws Exception {
+        Account acc = prepareProPayUseForGet("{\"account\":\"user\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}");
+        ArrayList<Reservation> reservations = acc.getReservations();
+        Assertions.assertThat(acc.getAccount()).isEqualTo("user");
         Assertions.assertThat(acc.getAmount()).isEqualTo(50);
         Assertions.assertThat(reservations.get(0).getId()).isEqualTo(1);
         Assertions.assertThat(reservations.get(0).getAmount()).isEqualTo(30);
@@ -61,49 +53,60 @@ public class ProPayTest {
     }
 
     @Test
-    public void checkReservationId() throws Exception{
-    	String usernameSource = "user";
-    	String usernameTarget = "user2";
-    	int amount = 30;
-    	String json = "{\"account\":\"user\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
-    	Mockito.when(pps.callURL("http://localhost:8888/reservation/reserve/" + usernameSource + "/" + usernameTarget + "?amount=" + Integer.toString(amount), "POST")).thenReturn(-1);
-    	prepareProPayUseForGet(json);
-    	Item item = new Item();
-    	item.setDeposit(30);
-    	pps.createDeposit("user", "user2", item);
-    	Item item2 = itemRepo.findById(3L).get();
-    	Assertions.assertThat(item2.getId()).isEqualTo(3L);
+    public void checkReservationId() throws Exception {
+        String usernameSource = "user";
+        String usernameTarget = "user2";
+        int amount = 30;
+        String json = "{\"account\":\"user\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
+        Mockito.when(pps.callURL("http://localhost:8888/reservation/reserve/" + usernameSource + "/" + usernameTarget + "?amount=" + Integer.toString(amount), "POST")).thenReturn(-1);
+        prepareProPayUseForGet(json);
+        Item item = new Item();
+        item.setDeposit(30);
+        pps.createDeposit("user", "user2", item);
+        Item item2 = itemRepo.findById(3L).get();
+        Assertions.assertThat(item2.getId()).isEqualTo(3L);
         Assertions.assertThat(item2.getReservationId()).isEqualTo(2);
     }
 
     @Test
-    public void checkReservationIdIsMinusOneForCancel() throws Exception{
-    	String usernameSource = "user3";
-    	int reservationId = 1;
-    	String json = "{\"account\":\"user3\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
-    	Mockito.when(pps.callURL("http://localhost:8888/reservation/release/" + usernameSource + "?reservationId=" + Integer.toString(reservationId), "POST")).thenReturn(-1);
-    	prepareProPayUseForGet(json);
-    	Item item3 = new Item();
-    	item3.setDeposit(30);
-    	pps.cancelDeposit("user3", item3);
-    	Item item4 = itemRepo.findById(2L).get();
-    	Assertions.assertThat(item4.getId()).isEqualTo(2L);
+    public void checkReservationIdIsMinusOneForCancel() throws Exception {
+        String usernameSource = "user3";
+        int reservationId = 1;
+        String json = "{\"account\":\"user3\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
+        Mockito.when(pps.callURL("http://localhost:8888/reservation/release/" + usernameSource + "?reservationId=" + Integer.toString(reservationId), "POST")).thenReturn(-1);
+        prepareProPayUseForGet(json);
+        Item item3 = new Item();
+        item3.setDeposit(30);
+        pps.cancelDeposit("user3", item3);
+        Item item4 = itemRepo.findById(2L).get();
+        Assertions.assertThat(item4.getId()).isEqualTo(2L);
         Assertions.assertThat(item4.getReservationId()).isEqualTo(-1);
     }
 
     @Test
-    public void checkReservationIdIsMinusOneForCollect() throws Exception{
-    	String usernameSource = "user3";
-    	int reservationId = 1;
-    	String json = "{\"account\":\"user3\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
-    	Mockito.when(pps.callURL("http://localhost:8888/reservation/punish/" + usernameSource + "?reservationId=" + Integer.toString(reservationId), "POST")).thenReturn(-1);
-    	prepareProPayUseForGet(json);
-    	Item item3 = new Item();
-    	item3.setDeposit(30);
-    	pps.collectDeposit("user3", item3);
-    	Item item4 = itemRepo.findById(1L).get();
-    	Assertions.assertThat(item4.getId()).isEqualTo(1L);
+    public void checkReservationIdIsMinusOneForCollect() throws Exception {
+        String usernameSource = "user3";
+        int reservationId = 1;
+        String json = "{\"account\":\"user3\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
+        Mockito.when(pps.callURL("http://localhost:8888/reservation/punish/" + usernameSource + "?reservationId=" + Integer.toString(reservationId), "POST")).thenReturn(-1);
+        prepareProPayUseForGet(json);
+        Item item3 = new Item();
+        item3.setDeposit(30);
+        pps.collectDeposit("user3", item3);
+        Item item4 = itemRepo.findById(1L).get();
+        Assertions.assertThat(item4.getId()).isEqualTo(1L);
         Assertions.assertThat(item4.getReservationId()).isEqualTo(-1);
+    }
+
+    @Test
+    public void checkNullPointers() throws Exception {
+        String usernameSource = "user3";
+        int reservationId = 1;
+        String json = "{\"account\":\"user3\",\"amount\":50.0,\"reservations\":[{\"id\":1,\"amount\":30.0},{\"id\":2,\"amount\":15.0}]}";
+        Mockito.when(pps.showAccount("user4")).thenReturn(null);
+        prepareProPayUseForGet(json);
+        int response = pps.createAccount("user4");
+        Assertions.assertThat(response).isEqualTo(-1);
     }
 
 }
