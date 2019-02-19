9b6cc3fef51b7963e3db65d7cdd22c7087078d56
My Ky <m.k.huynh@gmx.de>
2019-02-19 12:22:03 +0100

    Add button redirecting to propay account (My Ky, Maren)

--- src/main/java/de/hhu/sharing/web/ProPayController.java
+++ src/main/java/de/hhu/sharing/web/ProPayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
+import de.hhu.sharing.services.ProPayService;
 import java.util.Optional;
 
 @Controller
