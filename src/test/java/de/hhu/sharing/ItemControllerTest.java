ddf24c770266303a973db00ac3ab4516a9ce6408
martin <m.breuer@uni-duesseldorf.de>
2019-02-20 09:45:42 +0100

    Fix ItemControllerTest (Julian, Martin)

--- src/test/java/de/hhu/sharing/ItemControllerTest.java
+++ src/test/java/de/hhu/sharing/ItemControllerTest.java
@@ -1,17 +1,13 @@
 package de.hhu.sharing;
 
-import de.hhu.sharing.data.ItemRepository;
-import de.hhu.sharing.data.RequestRepository;
 import de.hhu.sharing.data.UserRepository;
 import de.hhu.sharing.model.Address;
 import de.hhu.sharing.model.Item;
 import de.hhu.sharing.model.User;
-import de.hhu.sharing.security.SecurityConfig;
 import de.hhu.sharing.services.ItemService;
 import de.hhu.sharing.services.RequestService;
 import de.hhu.sharing.services.UserService;
 import de.hhu.sharing.web.ItemController;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -19,19 +15,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.util.NestedServletException;
 
 import java.time.LocalDate;
-import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ItemController.class)
@@ -102,57 +95,14 @@ public class ItemControllerTest {
         map.add("rental","42");
         map.add("deposit","41");
 
-        mvc.perform(MockMvcRequestBuilders.post("/saveItem").params(map))
-                .andExpect(MockMvcResultMatchers.status().is(302));
-    }
-/*
-    @Test (expected = NestedServletException.class)
-    @WithMockUser
-    public void retrieveStatusItemWithIdNotExistent() throws Exception{
-        Mockito.when(itemRepository.findById((long)1)).thenReturn(Optional.empty());
-
-        mvc.perform(MockMvcRequestBuilders.get("/item").param("id","1"));
-    }
-
-
-
-    /*@Test
-    @WithMockUser
-    public void retrieveStatusSaveItem() throws Exception{
-
-            mvc.perform(MockMvcRequestBuilders.get("/saveItem"))
-                    .andExpect(MockMvcResultMatchers.status().is(405));
-
-    }*/ /*
-
-    @Test
-    @WithMockUser
-    public void retrieveStatusDelete() throws Exception{
-        Item item = new Item();
-
-        Mockito.when(itemRepository.findById((long)1)).thenReturn(Optional.of(item));
-        mvc.perform(MockMvcRequestBuilders.get("/deleteItem").param("id","1"))
+        mvc.perform(MockMvcRequestBuilders.post("/saveItem").with(csrf()).params(map))
                 .andExpect(MockMvcResultMatchers.redirectedUrl("/account"));
     }
 
-    @Test(expected = NestedServletException.class)
+    @Test
     @WithMockUser
-    public void retrieveStatusDeleteNotExistent() throws Exception{
-        Item item = new Item();
-
-        Mockito.when(itemRepository.findById((long)1)).thenReturn(Optional.empty());
+    public void retrieveStatusDeleteItem() throws Exception{
         mvc.perform(MockMvcRequestBuilders.get("/deleteItem").param("id","1"));
-                //.andExpect(MockMvcResultMatchers.redirectedUrl("/account"));
     }
 
-    private Item getItemIdOne() {
-        Item item = new Item();
-        item.setId((long)1);
-        User user = new User();
-        Address address = new Address();
-        user.setAddress(address);
-        item.setLender(user);
-        return item;
-    }
-*/
 }
