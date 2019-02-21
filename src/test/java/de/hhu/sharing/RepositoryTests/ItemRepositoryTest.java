f34382cf453b0747cf69d20d22b219dc7525c90e
martin <m.breuer@uni-duesseldorf.de>
2019-02-21 15:30:41 +0100

    Add RepositoryTest(Sarah,Martin)

--- src/test/java/de/hhu/sharing/RepositoryTests/ItemRepositoryTest.java
+++ src/test/java/de/hhu/sharing/RepositoryTests/ItemRepositoryTest.java
@@ -1,10 +1,9 @@
 package de.hhu.sharing.RepositoryTests;
 
 import de.hhu.sharing.data.ItemRepository;
+import de.hhu.sharing.data.RequestRepository;
 import de.hhu.sharing.data.UserRepository;
-import de.hhu.sharing.model.Address;
-import de.hhu.sharing.model.Item;
-import de.hhu.sharing.model.User;
+import de.hhu.sharing.model.*;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Test;
@@ -24,18 +23,40 @@ public class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepo;
+
     @Autowired
     UserRepository userRepo;
 
+    @Autowired
+    RequestRepository requestRepo;
+
+    public Request createRequest(User user){
+
+        LocalDate startdate = LocalDate.of(2010,1,1);
+        LocalDate enddate = LocalDate.of(2010,2,2);
+        Period period = new Period(startdate,enddate);
+
+        Request request = new Request(period,user);
+        requestRepo.save(request);
+        Request requestEntity = requestRepo.findById(request.getId()).get();
+        requestRepo.save(requestEntity);
+
+        return request;
+    }
+
+    public User createUser(String username){
+        LocalDate date = LocalDate.of(2000,1,1);
+        Address address = new Address("unistrase","duesseldorf", 40233);
+        User user = new User(username,"password", "role", "lastnmae", "forname", "email",date,address);
+        userRepo.save(user);
+        User userEntity = userRepo.findByUsername(username).get();
+        userRepo.save(userEntity);
+        return user;
+    }
 
 
     public ArrayList<Item> createItems(){
-        LocalDate date = LocalDate.of(2000,1,1);
-        Address address = new Address("unistrase","duesseldorf", 40233);
-        User user = new User("user","password", "role", "lastnmae", "forname", "email",date,address);
-        userRepo.save(user);
-        User userEntity = userRepo.findByUsername("user").get();
-        userRepo.save(userEntity);
+        User user = createUser("testman");
         Item item1 = new Item("apfel", "lecker",1,1 ,user );
         Item item2 = new Item("granatapfel", "grosse Kerne",2,2 ,user );
         Item item3 = new Item("banane", "kleine Kerne",3,3 ,user );
@@ -91,6 +112,65 @@ public class ItemRepositoryTest {
         Assertions.assertThat(itemList1.size()).isEqualTo(2);
         Assertions.assertThat(itemList2.size()).isEqualTo(2);
 
+    }
 
+    @Test
+    public void testFindFirst2ByLenderNot(){
+        ArrayList<Item> items = createItems();
+        itemRepo.save(items.get(0));
+        itemRepo.save(items.get(1));
+        itemRepo.save(items.get(2));
+
+        User user = createUser("superTestMan");
+        List<Item> itemList = itemRepo.findFirst2ByLenderNot(user);
+        Assertions.assertThat(itemList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void tesFindByRequests_id(){
+        ArrayList<Item> items = createItems();
+        Item item = items.get(0);
+
+        User user = createUser("superTestMan");
+
+        LocalDate startdate = LocalDate.of(2010,1,1);
+        LocalDate enddate = LocalDate.of(2010,2,2);
+        Period period = new Period(startdate,enddate);
+
+        Request request = createRequest(user);
+        item.addToRequests(request);
+
+        itemRepo.save(item);
+
+        Optional<Item> optionalItem = itemRepo.findByRequests_id(items.get(0).getRequests().get(0).getId());
+        Assertions.assertThat(optionalItem.get()).isEqualTo(items.get(0));
+    }
+
+    @Test
+    public void testFindFirstByLender(){
+        ArrayList<Item> items = createItems();
+        itemRepo.save(items.get(0));
+        itemRepo.save(items.get(1));
+
+        Optional <Item> optionalItem = itemRepo.findFirstByLender(items.get(0).getLender());
+        Assertions.assertThat(optionalItem.get()).isEqualTo(items.get(0));
+    }
+
+    @Test
+    public void TestFindAllByRequests_requester(){
+        ArrayList<Item> items = createItems();
+        Item item = items.get(0);
+        itemRepo.save(item);
+
+        User user = createUser("superTestMan");
+
+       Request request = createRequest(user);
+
+        item.addToRequests(request);
+
+        itemRepo.save(item);
+
+        List <Item> itemList = itemRepo.findAllByRequests_requester(user);
+        Assertions.assertThat(itemList.size()).isEqualTo(1);
     }
 }
