3eb6fa7f23ccf5e07ea7d753af9f35b1a3632ff5
martin <m.breuer@uni-duesseldorf.de>
2019-02-22 11:45:35 +0100

    Refactor RequestRepositoryTest(Sarah,Martin)

--- src/test/java/de/hhu/sharing/RepositoryTests/RequestRepositoryTest.java
+++ src/test/java/de/hhu/sharing/RepositoryTests/RequestRepositoryTest.java
@@ -27,18 +27,19 @@ public class RequestRepositoryTest {
     public ArrayList<Request> createRequests(){
 
         ArrayList<Request> requests= new ArrayList<>();
+
         LocalDate startdate = LocalDate.of(2000,1,1);
         LocalDate enddate=  LocalDate.of(2000,2,2);
         LocalDate date =  LocalDate.of(1996,2,12);
         Period period = new Period(startdate, enddate);
         
         Address address = new Address("unistrase","duesseldorf", 40233);
-        User user1 = new User("user1","password","role","lastname","forname", "email", date, address);
-        User user2 = new User("user2","password","role","lastname","forname", "email", date, address);
-        userRepo.save(user1);
-        userRepo.save(user2);
-        User requester1 = userRepo.findByUsername("user1").get();
-        User requester2 = userRepo.findByUsername("user2").get();
+        User requester1 = new User("user1","password","role","lastname","forname", "email", date, address);
+        User requester2 = new User("user2","password","role","lastname","forname", "email", date, address);
+        userRepo.save(requester1);
+        userRepo.save(requester2);
+        userRepo.findByUsername("user1").get();
+        userRepo.findByUsername("user2").get();
         Request request1 = new Request(period,requester1 );
         Request request2 = new Request(period ,requester2 );
         Request request3 = new Request(period ,requester1 );
@@ -52,7 +53,9 @@ public class RequestRepositoryTest {
 
     @Test
     public void testFindAll(){
+
         ArrayList<Request> requests = createRequests();
+
         reqRepo.save(requests.get(0));
         reqRepo.save(requests.get(1));
 
@@ -62,10 +65,12 @@ public class RequestRepositoryTest {
     public void testFindAllByRequester() {
 
         ArrayList<Request> requests = createRequests();
+
         reqRepo.save(requests.get(0));
         reqRepo.save(requests.get(1));
         reqRepo.save(requests.get(2));
         User user = requests.get(0).getRequester();
+        
         Assertions.assertThat(reqRepo.findAllByRequester(user).size()).isEqualTo(2);
     }
 }
