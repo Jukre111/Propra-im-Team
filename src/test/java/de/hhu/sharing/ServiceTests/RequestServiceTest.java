08441883f11b14b41efdcb45e4702144b8c8c1e0
SarahLuca <findus.luca@yahoo.de>
2019-02-22 11:58:38 +0100

    Add tests RequestService

--- src/test/java/de/hhu/sharing/ServiceTests/RequestServiceTest.java
+++ src/test/java/de/hhu/sharing/ServiceTests/RequestServiceTest.java
@@ -1,16 +1,12 @@
 package de.hhu.sharing.ServiceTests;
-import de.hhu.sharing.data.ItemRepository;
 import de.hhu.sharing.data.RequestRepository;
 import de.hhu.sharing.model.*;
 import de.hhu.sharing.services.ItemService;
 import de.hhu.sharing.services.RequestService;
-import de.hhu.sharing.services.TransactionService;
-import de.hhu.sharing.services.UserService;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.*;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -34,35 +30,34 @@ public class RequestServiceTest {
     }
 
 
-    private User generateUser() {
+    private User generateUser(String username) {
         LocalDate birthdate = LocalDate.of(2000,1,1);
         Address address = new Address("unistrase","duesseldorf", 40233);
-        return new User("user","password", "role", "lastname", "forename", "email", birthdate, address);
+        return new User(username,"password", "role", "lastname", "forename", "email", birthdate, address);
     }
 
 
-    private Item generateItem() {
-        User user = generateUser();
+    private Item generateItem(User user) {
         return new Item("apfel", "lecker",1,1 ,user );
     }
 
-    private Request generateRequest() {
-        User requester = generateUser();
+    private Request generateRequest(User requester) {
         Period period = new Period(LocalDate.of(2000,2,2),LocalDate.of(2000,2,3));
         return new Request(period ,requester);
     }
 
     @Test
     public void testGet(){
-        Request request = generateRequest();
+        User user = generateUser("Karl");
+        Request request = generateRequest(user);
         Mockito.when(requests.findById(1L)).thenReturn(Optional.of(request));
         Assert.assertEquals(requestService.get(1L), request);
     }
 
     @Test
     public void testCreate(){
-        User user = generateUser();
-        Item item = generateItem();
+        User user = generateUser("Karl");
+        Item item = generateItem(user);
         Mockito.when(itemService.get(1L)).thenReturn(item);
 
         requestService.create(1L, LocalDate.of(2000,2,2),LocalDate.of(2000,2,3),user);
@@ -76,9 +71,11 @@ public class RequestServiceTest {
 
     @Test
     public void testDelete(){
-        Request request = generateRequest();
+        User user1 = generateUser("user");
+        User user2 = generateUser("user");
+        Request request = generateRequest(user1);
         request.setId(1L);
-        Item item = generateItem();
+        Item item = generateItem(user2);
         item.addToRequests(request);
         Mockito.when(requests.findById(1L)).thenReturn(Optional.of(request));
         Mockito.when(itemService.getFromRequestId(1L)).thenReturn(item);
@@ -90,6 +87,31 @@ public class RequestServiceTest {
 
         Assert.assertEquals(captor.getAllValues().get(0), request);
         Assert.assertTrue(item.getRequests().isEmpty());
+    }
+
+    @Test
+    public void testDeleteOverlappingRequestsFromItem(){
+        User requester = generateUser("requester");
+        User owner = generateUser("owner");
+        User otherRequester = generateUser("otherRequester");
+
+        Request request = generateRequest(requester);
+        request.setId(1L);
+        Request otherRequest = generateRequest(otherRequester);
+        otherRequest.setId(2L);
+        Item item = generateItem(owner);
+        item.addToRequests(otherRequest);
+
+        Mockito.when(requests.findById(1L)).thenReturn(Optional.of(request));
+        Mockito.when(requests.findById(2L)).thenReturn(Optional.of(otherRequest));
+        Mockito.when(itemService.getFromRequestId(1L)).thenReturn(item);
+        Mockito.when(itemService.getFromRequestId(2L)).thenReturn(item);
+
+        requestService.deleteOverlappingRequestsFromItem(request, item);
+
+        Assert.assertTrue(item.getRequests().isEmpty());
+
+
     }
 
 }
