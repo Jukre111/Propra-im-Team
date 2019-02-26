7c8b9cb1452c2d3ee291500d94559a417c108ab0
martin <m.breuer@uni-duesseldorf.de>
2019-02-26 11:15:14 +0100

    Refactor UserServiceTest(Julian,Martin)

--- src/test/java/de/hhu/sharing/ServiceTests/UserServiceTest.java
+++ src/test/java/de/hhu/sharing/ServiceTests/UserServiceTest.java
@@ -2,6 +2,8 @@ package de.hhu.sharing.ServiceTests;
 
 import de.hhu.sharing.data.UserRepository;
 import de.hhu.sharing.model.Address;
+import de.hhu.sharing.model.BorrowingProcess;
+import de.hhu.sharing.model.Item;
 import de.hhu.sharing.model.User;
 import de.hhu.sharing.services.UserService;
 import org.junit.Assert;
@@ -13,6 +15,8 @@ import org.springframework.web.util.NestedServletException;
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
 
 public class UserServiceTest {
 
@@ -22,33 +26,38 @@ public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
-    @Before public void initialize(){
+    @Before
+    public void initialize() {
         MockitoAnnotations.initMocks(this);
     }
 
+    private Item generateItem(User user) {
+        return new Item("apfel", "lecker", 1, 1, user);
+    }
+
     public User generateUser() {
-        LocalDate date = LocalDate.of(2000,1,1);
-        Address address = new Address("unistrase","duesseldorf", 40233);
-        User user = new User("user","password", "role", "lastnmae", "forname", "email",date,address);
+        LocalDate date = LocalDate.of(2000, 1, 1);
+        Address address = new Address("unistrase", "duesseldorf", 40233);
+        User user = new User("user", "password", "role", "lastnmae", "forname", "email", date, address);
         return user;
     }
 
     @Test
-    public void testGet(){
+    public void testGet() {
         User user = generateUser();
         Mockito.when(users.findByUsername("user")).thenReturn(Optional.of(user));
         User testUser = userService.get("user");
         Assert.assertTrue(testUser.equals(user));
     }
 
-    @Test (expected = RuntimeException.class)
-    public void testGetNoUserFound(){
+    @Test(expected = RuntimeException.class)
+    public void testGetNoUserFound() {
         Mockito.when(users.findByUsername("user")).thenReturn(Optional.empty());
         userService.get("user");
     }
 
     @Test
-    public void testGetBorrowerFromBorrowingProcessId(){
+    public void testGetBorrowerFromBorrowingProcessId() {
 
         User user = generateUser();
         Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user));
@@ -56,37 +65,77 @@ public class UserServiceTest {
         Assert.assertTrue(testUser.equals(user));
     }
 
-    @Test (expected = RuntimeException.class)
-    public void testGetBorrowerFromBorrowingProcessIdNoUserFound(){
+    @Test(expected = RuntimeException.class)
+    public void testGetBorrowerFromBorrowingProcessIdNoUserFound() {
         Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.empty());
         userService.getBorrowerFromBorrowingProcessId(1L);
     }
 
 
-   // @Test
-  //  public void testRemoveProcessFromProcessLists(){
+    @Test
+    public void testRemoveProcessFromProcessLists() {
 
-      //  User user = generateUser();
-      //  Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user));
 
-   // }
-//    Method doesn't exist anymore
-//    @Test
-//    public void testAddToBorrowedItems(){
-//        User user = generateUser();
-//
-//        Item item = new Item("apfel", "lecker",1,1 ,user );
-//        Item item2 = new Item("apfel", "lecker",1,1 ,user );
-//        Item item3 = new Item("apfel", "lecker",1,1 ,user );
-//        ArrayList<Item> liste = new ArrayList<>();
-//        liste.add(item);
-//        liste.add(item2);
-//        liste.add(item3);
-//        userService.addToBorrowedItems(user,item);
-//        userService.addToBorrowedItems(user,item2);
-//        userService.addToBorrowedItems(user,item3);
-//        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-//        Mockito.verify(users, times(3)).save(captor.capture());
-//        Assert.assertTrue(captor.getAllValues().get(0).getBorrowedItems().equals(liste));
-//    }
+        User user1 = generateUser();
+        User user2 = generateUser();
+        Item item = generateItem(user1);
+
+        BorrowingProcess process = new BorrowingProcess();
+        process.setId(1L);
+        process.setItem(item);
+        user1.addToLend(process);
+        user2.addToBorrowed(process);
+        Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user2));
+        userService.removeProcessFromProcessLists(process);
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(users, times(2)).save(captor.capture());
+
+
+        Assert.assertTrue(captor.getAllValues().get(0).getLend().size() == 0);
+        Assert.assertTrue(captor.getAllValues().get(1).getBorrowed().size() == 0);
+
+    }
+
+    @Test
+    public void testUserIsLender() {
+        User user1 = generateUser();
+        User user2 = generateUser();
+        Item item = generateItem(user1);
+        BorrowingProcess process = new BorrowingProcess();
+        process.setId(1L);
+        process.setItem(item);
+        Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user2));
+
+        Assert.assertTrue(userService.userIsInvolvedToProcess(user1, process) == true);
+    }
+
+    @Test
+    public void testUserIsBorrower() {
+        User user1 = generateUser();
+        User user2 = generateUser();
+
+        Item item = generateItem(user1);
+        BorrowingProcess process = new BorrowingProcess();
+        process.setId(1L);
+        process.setItem(item);
+        user2.addToBorrowed(process);
+        Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user2));
+
+        Assert.assertTrue(userService.userIsInvolvedToProcess(user2, process) == true);
+    }
+
+    @Test
+    public void testUserIsNotInvoved() {
+        User user1 = generateUser();
+        User user2 = generateUser();
+
+        Item item = generateItem(user1);
+        BorrowingProcess process = new BorrowingProcess();
+        process.setItem(item);
+        process.setId(1L);
+        Mockito.when(users.findByBorrowed_id(1L)).thenReturn(Optional.of(user1));
+
+        Assert.assertTrue(userService.userIsInvolvedToProcess(user2, process) == false);
+    }
+
 }
