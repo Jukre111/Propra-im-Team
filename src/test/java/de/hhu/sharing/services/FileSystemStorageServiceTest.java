6fa2d9347eaa79d22f95e77f49a3b3106c449174
SarahLuca <findus.luca@yahoo.de>
2019-02-27 12:07:56 +0100

    Refactor tests for FileSystemStorageService (Martin, Sarah)

--- src/test/java/de/hhu/sharing/services/FileSystemStorageServiceTest.java
+++ src/test/java/de/hhu/sharing/services/FileSystemStorageServiceTest.java
@@ -1,11 +1,13 @@
 package de.hhu.sharing.services;
 
+import static org.mockito.internal.verification.VerificationModeFactory.noMoreInteractions;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 import java.nio.charset.Charset;
 import java.time.LocalDate;
 
 import de.hhu.sharing.data.LendableItemRepository;
+import de.hhu.sharing.data.SellableItemRepository;
 import de.hhu.sharing.model.LendableItem;
 import org.junit.Assert;
 import org.junit.Before;
@@ -22,19 +24,20 @@ import de.hhu.sharing.data.UserRepository;
 import de.hhu.sharing.model.Address;
 import de.hhu.sharing.model.Image;
 import de.hhu.sharing.model.User;
-import de.hhu.sharing.services.FileSystemStorageService;
 
 public class FileSystemStorageServiceTest {
-
-
-    @Mock
-    private LendableItemRepository items;
     
     @Mock
     private ImageRepository images;
     
     @Mock
     private UserRepository users;
+
+    @Mock
+    private LendableItemRepository items;
+
+    @Mock
+    private SellableItemRepository sellableItemRepository;
 
     @InjectMocks
     private FileSystemStorageService fileStorageService;
@@ -46,33 +49,55 @@ public class FileSystemStorageServiceTest {
     private User generateUser(String username) {
         LocalDate birthdate = LocalDate.of(2000,1,1);
         Address address = new Address("unistrase","duesseldorf", 40233);
-        User user = new User(username,"password", "role", "lastname", "forename", "email", birthdate, address);
-        return user;
+        return new User(username,"password", "role", "lastname", "forename", "email", birthdate, address);
     }
 
     private LendableItem generateItem(User user) {
         return new LendableItem("apfel", "lecker", 1, 1, user);
     }
-    
+
     @Test
-    public void testCreateImageVars(){
-    	String mimetype = "image/gif";
-    	Image image = fileStorageService.createImageVars(mimetype);
-    	Assert.assertEquals(image.getMimeType(), mimetype);
-    	mimetype = "image/bmp";
-    	image = fileStorageService.createImageVars(mimetype);
-    	Assert.assertEquals(image.getMimeType(), mimetype);
-    	mimetype = "image/jpeg";
-    	image = fileStorageService.createImageVars(mimetype);
-    	Assert.assertEquals(image.getMimeType(), mimetype);
-    	mimetype = "image/png";
-    	image = fileStorageService.createImageVars(mimetype);
-    	Assert.assertEquals(image.getMimeType(), mimetype);
-    	mimetype = "irgendwas";
-    	image = fileStorageService.createImageVars(mimetype);
-    	Assert.assertEquals(image.getMimeType(), null);
+    public void testCreateImageVarsJpeg() {
+        String mimetype = "image/jpeg";
+        Image image = fileStorageService.createImageVars(mimetype);
+        Assert.assertEquals(image.getMimeType(), mimetype);
     }
-    
+
+    @Test
+    public void testCreateImageVarsPng() {
+        String mimetype = "image/png";
+        Image image = fileStorageService.createImageVars(mimetype);
+        Assert.assertEquals(image.getMimeType(), mimetype);
+    }
+
+    @Test
+    public void testCreateImageVarsGif() {
+        String mimetype = "image/gif";
+        Image image = fileStorageService.createImageVars(mimetype);
+        Assert.assertEquals(image.getMimeType(), mimetype);
+    }
+
+    @Test
+    public void testCreateImageVarsBmp() {
+        String mimetype = "image/bmp";
+        Image image = fileStorageService.createImageVars(mimetype);
+        Assert.assertEquals(image.getMimeType(), mimetype);
+    }
+
+    @Test
+    public void testCreateImageVarsDefault() {
+        String mimetype = "anyString";
+        Image image = fileStorageService.createImageVars(mimetype);
+        Assert.assertNull(image.getMimeType());
+    }
+
+//    @Test
+//    public void testReadFileNoException(){
+//        MockMultipartFile jsonFile = new MockMultipartFile("test.json", "", "application/json", "{\"key1\": \"value1\"}".getBytes(Charset.forName("UTF-8")));
+//
+//        fileStorageService.
+//    }
+
 //    @Test
 //    public void testStoreUserInitalizer() throws Exception{
 //    	User user = generateUser("user");
@@ -105,20 +130,40 @@ public class FileSystemStorageServiceTest {
 //    }
     
     @Test
-    public void testStoreUser() throws Exception{
+    public void testStoreUserContentTypeNotNull() throws Exception{
     	User user = generateUser("user");
     	MockMultipartFile jsonFile = new MockMultipartFile("test.gif", "", "image/gif", "{\"key1\": \"value1\"}".getBytes(Charset.forName("UTF-8")));         
     	fileStorageService.storeUser(jsonFile, user);
         ArgumentCaptor<Image> captor = ArgumentCaptor.forClass(Image.class);
         Mockito.verify(images, times(1)).save(captor.capture());
-        Assert.assertEquals(captor.getAllValues().get(0).getMimeType(), "image/gif");
-        Assert.assertEquals(captor.getAllValues().get(0).getImageData().length, jsonFile.getBytes().length);
         ArgumentCaptor<User> captor2 = ArgumentCaptor.forClass(User.class);
         Mockito.verify(users, times(1)).save(captor2.capture());
-        Assert.assertEquals(captor2.getAllValues().get(0).getRole(), "role");
-        Assert.assertEquals(captor2.getAllValues().get(0).getUsername(), "user");
+
+        Assert.assertEquals(captor.getValue().getMimeType(), "image/gif");
+        Assert.assertEquals(captor.getValue().getImageData().length, jsonFile.getBytes().length);
+        Assert.assertEquals(captor2.getValue().getRole(), "role");
+        Assert.assertEquals(captor2.getValue().getUsername(), "user");
+        Assert.assertEquals(captor2.getValue().getImage(), captor.getValue());
     }
-    
+
+    @Test
+    public void testStoreUserContentTypeNull() throws Exception{
+        User user = generateUser("user");
+        MockMultipartFile jsonFile = new MockMultipartFile("test.gif", "", null, "{\"key1\": \"value1\"}".getBytes(Charset.forName("UTF-8")));
+        fileStorageService.storeUser(jsonFile, user);
+        ArgumentCaptor<Image> captor = ArgumentCaptor.forClass(Image.class);
+        Mockito.verify(images, times(1)).save(captor.capture());
+        ArgumentCaptor<User> captor2 = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(users, times(1)).save(captor2.capture());
+
+        Assert.assertNull(captor.getValue().getMimeType());
+        Assert.assertEquals(captor.getValue().getImageData().length, jsonFile.getBytes().length);
+        Assert.assertEquals(captor2.getValue().getRole(), "role");
+        Assert.assertEquals(captor2.getValue().getUsername(), "user");
+        Assert.assertEquals(captor2.getValue().getImage(), captor.getValue());
+    }
+
+
     @Test
     public void testStoreItem() throws Exception{
     	User user = generateUser("user");
